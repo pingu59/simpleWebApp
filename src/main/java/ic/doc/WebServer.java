c5f3f3144d39058f70b21d3d70b3725e67c229fb
Tianyi Zuo <tz2617@point38.doc.ic.ac.uk>
2019-01-15 21:04:18 +0000

    Do not know how to test

--- src/main/java/ic/doc/WebServer.java
+++ src/main/java/ic/doc/WebServer.java
@@ -1,9 +1,7 @@
 package ic.doc;
 
-
 import ic.doc.web.HTMLResultPage;
 import ic.doc.web.IndexPage;
-import java.io.File;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -16,7 +14,7 @@ import java.io.IOException;
 public class WebServer {
 
     public WebServer() throws Exception {
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        Server server = new Server(5000);//Integer.valueOf(System.getenv("PORT")));
 
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(new ServletHolder(new Website()), "/*");
@@ -27,21 +25,26 @@ public class WebServer {
 
     static class Website extends HttpServlet {
         @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
             String query = req.getParameter("q");
             String type = req.getParameter("t");
-            if (query == null) {
-                new IndexPage().writeTo(resp);
-            } else {
-                String result = new QueryProcessor().process(query);
-                if(type == null){
-                    new HTMLResultPage(query, result).writeTo(resp);
-                }else if (type.equals("markdown")){
-                    new MarkDownCreator(query, result).writeTo(resp);
-                }else{
-                    //PDF
+            execute(query, type, resp);
 
-                }
+        }
+    }
+
+    public static void execute(String query, String type, HttpServletResponse resp)throws IOException{
+        if (query == null) {
+            new IndexPage().writeTo(resp);
+        } else {
+            String result = new QueryProcessor().process(query);
+            if(type == null){
+                new HTMLResultPage(query, result).writeTo(resp);
+            }else if (type.equals("markdown")){
+                System.out.println("Is a markdown");
+                new MarkDownCreator(query, result).writeTo(resp);
+            }else{
+                //PDF
             }
         }
     }
---
+++ src/test/java/ic/doc/MarkDownTest.java
@@ -0,0 +1,13 @@
+package ic.doc;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class MarkDownTest  {
+
+  @Test
+  public void markdownRadioButtonWorking() throws Exception {
+    //TODO: I dont know how to test
+  }
+}
